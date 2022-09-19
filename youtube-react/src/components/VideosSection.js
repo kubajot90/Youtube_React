f87c5c2200f311145aa62c974c456f946438aaf4
Jakub Jakubowski <kubajot6@wp.pl>
2022-09-19 17:06:11 +0100

    Add date counter.

--- youtube-react/src/components/VideosSection.js
+++ youtube-react/src/components/VideosSection.js
@@ -14,26 +14,9 @@ import classes from './VideosSection.module.css';
 
     useEffect(()=>{
         const fetchVideos = ()=>{
-        // const fetchVideos = async ()=>{
-            // const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&videoEmbeddable=true&order=viewCount&q=${search}&type=video&part=snippet&maxResults=4`);
-
-            // const responseData= await response.json();
-            // console.log('responseData.items');
-            // console.log(responseData.items);
-           
-            // setVideos(responseData.items);
-        console.log('start fetch data==================');
             fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&videoEmbeddable=true&order=viewCount&q=${search}&type=video&part=snippet&maxResults=4`).then((response)=>response.json()).then((responseData)=>{
-                console.log('responseData.items');
-                console.log(responseData.items);
-                console.log('responseData.items.length');
-                console.log(responseData.items.length);
-                // responseData.items.length && setVideos(responseData.items);
                 if(responseData.items.length){
                     setVideos(responseData.items)
-                    console.log('length wiekszy od 0, set odpalone');
-                    console.log('videos.length');
-                    console.log(videos.length);
                 }
                 
             })
@@ -46,10 +29,7 @@ import classes from './VideosSection.module.css';
 
 
     useEffect( () => {
-        console.log('useEFFECT');
-        console.log(videos);
         if(videos.length){
-            console.log('fetchurl sie odpala');
             setChannelsIdArr();
             fetchUrl() 
            
@@ -59,8 +39,6 @@ import classes from './VideosSection.module.css';
      [videos])
 
     const setChannelsIdArr =()=>{
-        console.log('setchannelid');
-        // console.log(videos);
         videos.forEach((video)=>(
             setChannelIds(oldArray => [...oldArray, video.snippet.channelId]))
         )
@@ -70,12 +48,10 @@ import classes from './VideosSection.module.css';
         const response = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?key=${apiKey}&part=snippet&part=statistics&id=${id}`);
         const responseData = await response.json();
         const url = responseData.items[0].snippet.thumbnails.medium.url;
-        console.log('responseData fetch profile img');
-        console.log(responseData);
+      
         const views = responseData.items[0].statistics.viewCount;
         const updatedValues = {[id]:{imgUrl: url, videoViews : views}};
-        // const updatedValues = {[id]: url};
-        
+       
           setProfilesImgObj(prevState => {
             return {...prevState, ...updatedValues};
           });
@@ -83,17 +59,12 @@ import classes from './VideosSection.module.css';
 
    
     useEffect(()=>{
-        console.log('111111111111111111111profilesImgObj');
-        console.log(profilesImgObj);
-        console.log(Object.keys(profilesImgObj).length);
         if(Object.keys(profilesImgObj).length === videos.length){
-            console.log('sssssssssssssssssssssssssssssssstartcreatecards');
             createCardsFunc()
         }
     },[profilesImgObj])
 
     const fetchUrl=()=>{
-        console.log('FETCHURL');
         channelIds.forEach((id=>{ 
                 fetchProfileImgUrl(id)
             }
@@ -102,24 +73,11 @@ import classes from './VideosSection.module.css';
     
     const createCardsFunc=()=>{
         const createNewCards = videos.map((video)=>{
-            console.log('createCards videos.length');
-            console.log(videos.length);
-            console.log(videos);
-            console.log('profilesImgObj');
-            console.log(profilesImgObj);
-            // console.log('channelID');
-            // console.log(video.snippet.channelId);
             const profileObj = profilesImgObj[video.snippet.channelId];
-    console.log('---------profileObj');
-    console.log(profileObj);
-    // console.log(profileObj.videoViews);
-           
     
            return <VideoCard thumbnailUrl={video.snippet.thumbnails.high.url} profileImgUrl={profileObj.imgUrl} title={video.snippet.title} channelTitle={video.snippet.channelTitle} key={video.id.videoId} date={video.snippet.publishedAt}  viewCount={profileObj.videoViews} />
         }
         )
-        console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxcreateCards');
-        console.log(createCards);
         setCreateCards(createNewCards)
     }
    
